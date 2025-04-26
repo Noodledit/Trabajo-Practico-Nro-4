0a917572891c21a50af7ea56668771555002f51f
Noodledit <Nicolas.White.py@proton.me>
2025-04-26 14:00:50 -0300

    Dantes Commit
    Se realizo la conexion con la base de datos VIAJES.

--- TP4_Grupo12/Ejercicio1.aspx.cs
+++ TP4_Grupo12/Ejercicio1.aspx.cs
@@ -4,14 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace TP4_Grupo12
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+        private const string connectionString = "Data Source=NOODLE-DESK;Initial Catalog=Viajes;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
+        private string sqlQueryProvincias = "SELECT * FROM Provincias";
         protected void Page_Load(object sender, EventArgs e)
         {
+            if(!IsPostBack)
+            {
 
+            }
         }
     }
 }

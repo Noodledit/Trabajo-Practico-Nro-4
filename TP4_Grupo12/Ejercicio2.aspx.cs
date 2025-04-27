b71fdfb8f69a8b86fce5875474fd9564c8ca6e4a
Miguel Velásquez <86170247+MrM1nd91@users.noreply.github.com>
2025-04-27 19:35:10 -0300

    Ultima Actualizacion

--- TP4_Grupo12/Ejercicio2.aspx.cs
+++ TP4_Grupo12/Ejercicio2.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,10 +10,22 @@ namespace TP4_Grupo12
 {
     public partial class WebForm2 : System.Web.UI.Page
     {
+
+        private const string connectionString = @"Server=(local);Database=Neptuno;Integrated Security=True";
+
+        private const string sqlQueryProductos = "SELECT * FROM Productos";
+
         protected void Page_Load(object sender, EventArgs e)
         {
+
+
             if (!IsPostBack)
             {
+
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                SqlCommand sqlCommand = new SqlCommand(sqlQueryProductos, connection);
+
                 ddlProductoId.Items.Clear();
                 ddlCategoriaId.Items.Clear();
 
@@ -22,10 +35,19 @@ namespace TP4_Grupo12
                 ddlProductoId.Items.Add(new ListItem("Mayor a:", "2"));
                 ddlProductoId.Items.Add(new ListItem("Menor a:", "3"));
 
-                // Asignar valores al DropDownList IDCategoria
+                // Asignar valores al DropDownList ddl
                 ddlCategoriaId.Items.Add(new ListItem("Igual a:", "1"));
                 ddlCategoriaId.Items.Add(new ListItem("Mayor a:", "2"));
                 ddlCategoriaId.Items.Add(new ListItem("Menor a:", "3"));
+
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+
+                //GridView1.DataSource = reader;
+
+                reader.Close();
+
+                connection.Close();
+
             }
         }
 
@@ -38,4 +60,5 @@ namespace TP4_Grupo12
 
 
     }
+
 }

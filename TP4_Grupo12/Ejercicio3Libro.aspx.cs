752495612cc72debce1298f2e21a50afc6f15c08
ValenDuarte1918 <valentinduarte1@hotmail.com>
2025-04-28 18:35:01 -0300

    agregue consulta sql y sqlcommand

--- TP4_Grupo12/Ejercicio3Libro.aspx.cs
+++ TP4_Grupo12/Ejercicio3Libro.aspx.cs
@@ -21,10 +21,24 @@ namespace TP4_Grupo12
         {
             string connectionDB = "Data Source=localhost;Initial Catalog=Libreria;Integrated Security=True";
 
+            // Data Source=localhost;Initial Catalog=Libreria;Integrated Security=True";
+            // Data Source = DESKTOP - MHN7D94\\SQLEXPRESS; Initial Catalog = Libreria; Integrated Security = True
+
+            // consulta SQL para obtener los libros y sus temas. Usando INNER JOIN para combinar las tablas Libros y Temas. 
+            string sqlQueryTemas = 
+                "SELECT L.IdLibro, L.Titulo, L.Autor, L.Precio, T.Tema " +
+                "FROM Libros L " +
+                "INNER JOIN Temas T ON L.IdTema = T.IdTema " +
+                "WHERE L.IdLibro = @IdLibro";
+            // Data Source=DESKTOP-MHN7D94\SQLEXPRESS;Initial Catalog=Libreria;Integrated Security=True
+            // Data Source=localhost;Initial Catalog=Libreria;Integrated Security=True
+
             SqlConnection sqlConnectionBook = new SqlConnection(connectionDB);
             sqlConnectionBook.Open();
-
-
+            // se declara el Command para realizar la lectura de la base de datos
+            SqlCommand sqlCommand = new SqlCommand(sqlQueryTemas, sqlConnectionBook);
+            // se declaran los parametros para la consulta
+            sqlCommand.Parameters.AddWithValue("@IdLibro", Request.QueryString["idLibro"]);
         }
     }
 }

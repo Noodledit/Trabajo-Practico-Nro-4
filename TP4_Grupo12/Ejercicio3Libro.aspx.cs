ee49ed343e25fbf6dfe288f44b14035e4fb9c19d
Noodledit <Nicolas.White.py@proton.me>
2025-04-28 17:01:14 -0300

    Dantes Commit
    hice la conexion con la base de datos para el 2do webform y declare la funcion para cargar la base de datos

--- TP4_Grupo12/Ejercicio3Libro.aspx.cs
+++ TP4_Grupo12/Ejercicio3Libro.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace TP4_Grupo12
 {
@@ -11,7 +12,14 @@ namespace TP4_Grupo12
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                MostrarLibro();
+            }
+        }
+        private void MostrarLibro()
+        {
+            string connectionDB = "Data Source=localhost;Initial Catalog=Libreria;Integrated Security=True";
         }
     }
 }
